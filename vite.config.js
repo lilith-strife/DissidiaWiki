3b4bac44865c3405700fe0badaf3e93cd4f35082
lilith-strife <jasonc8484@gmail.com>
2026-04-24 06:39:36 -0500

    oops

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dissidia-items/',
+  base: '/dissidiawiki/',
   build: {
     outDir: 'dist',
     sourcemap: true
